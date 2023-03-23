e2b8137d729c3635762ec4f54a6b16c3afb95ec7
Your Name <rghzaiel@ggn-group.com>
2023-03-23 11:57:31 +0100

    contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,8 @@
+import { doc, getDoc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { db } from "../firebase.config";
 
 function Contact() {
   const [message, setMessage] = useState("");
@@ -8,8 +11,58 @@ function Contact() {
 
   const params = useParams();
 
-  useEffect(() => {});
-  return <div>Contact</div>;
+  useEffect(() => {
+    const getLandlord = async () => {
+      const docRef = doc(db, "users", params.landlordId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setLandlord(docSnap.data());
+      } else {
+        toast.error("Could not get landlord data");
+      }
+    };
+    getLandlord();
+  }, [params.landlordId]);
+
+  const onChange = (e) => setMessage(e.target.value);
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Conatct Landlord</p>
+      </header>
+      {landlord !== null && (
+        <main>
+          <div className="contactLandlord">
+            <p className="landlordName">Contact {landlord?.name}</p>
+          </div>
+          <form className="messageForm">
+            <div className="messageDiv">
+              <label htmlFor="message" className="messageLable">
+                Message
+              </label>
+              <textarea
+                name="message"
+                id="message"
+                className="textarea"
+                value={message}
+                onChange={onChange}
+              ></textarea>
+            </div>
+            <a
+              href={`mailto:${landlord.email}?Subject=${searchParams.get(
+                "listingName"
+              )}&body=${message}`}
+            >
+              <button type="button" className="primaryButton">
+                Send Message
+              </button>
+            </a>
+          </form>
+        </main>
+      )}
+    </div>
+  );
 }
 
 export default Contact;
